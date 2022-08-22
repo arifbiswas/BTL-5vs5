1f89cf77a47cfcd2462e9c4459eee5ab333f361d
arifbiswas <mxgangstar310@gmail.com>
2022-08-22 19:37:24 +0600

    js basic work start

---
+++ js/common.js
@@ -0,0 +1,18 @@
+function getInputValue(inputId) {
+    const inputFeild = document.getElementById(inputId);
+    const inputValueString = inputFeild.value;
+    const inputValue = parseFloat(inputValueString);
+    return inputValue;
+}
+
+function getElementValue(elementId) {
+    const element = document.getElementById(elementId);
+    const elementString = element.innerText;
+    const elementValue = parseFloat(elementString);
+    return elementValue;
+}
+function setElementValue(elementId, elementValue) {
+    const element = document.getElementById(elementId);
+    const elementString = element.innerText;
+    elementString = elementValue;
+}
---
+++ js/playerSelect.js
@@ -0,0 +1,3 @@
+function selectBtn() {
+    console.log('cliked');
+}
